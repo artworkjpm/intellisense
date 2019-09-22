96e3b35205fa5749cacdd84b0e5296a8f80e8d38
John Moran <johnnymoran44@hotmail.com>
2019-09-22 22:36:38 +0200

    3 datasets

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 export const NewYork = axios
-  .get("https://app.climate.azavea.com/api/city/1751", {
+  .get("https://app.climate.azavea.com/api/climate-data/1/RCP85/indicator/average_high_temperature/?years=2006:2030&units=C", {
     headers: {
       Authorization: "Token 1a740d6aef7f5638ca7ec085a938085e8218f5f5"
     }
   })
   .then(response => {
-    console.log(response);
+    return response.data.data;
   })
   .catch(error => {
     console.log(error);
